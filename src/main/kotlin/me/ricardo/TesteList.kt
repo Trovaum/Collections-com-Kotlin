e468289e11b8fed3d0f74623e95aea9e722e9c25
Trovaum <ricardopouza@gmail.com>
2022-01-04 16:44:45 -0300

    List sortedBy com encadeamento

--- src/main/kotlin/me/ricardo/TesteList.kt
+++ src/main/kotlin/me/ricardo/TesteList.kt
@@ -1,7 +1,7 @@
 package me.ricardo
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
+    val joao = Funcionario("João", 5000.0)
     val ricardo = Funcionario("Ricardo", 2000.0)
     val enzo = Funcionario("Enzo", 3000.0)
 
@@ -9,6 +9,8 @@ fun main() {
 
     funcionarios.forEach { println(it) }
     println("Busca por nome usando metodo find: " + funcionarios.find { it.nome == "Enzo" })
+
+    funcionarios.sortedBy { it.salario }.forEach { println("Usando ordenação por faixa salarial " + it) }
 
 }
 
