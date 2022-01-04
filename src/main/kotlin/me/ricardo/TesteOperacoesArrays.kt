56aee2c422e0325f192fb760aa8ff116625c039c
Trovaum <ricardopouza@gmail.com>
2022-01-04 14:11:52 -0300

    exercicio operaçoes com arrayDouble

--- src/main/kotlin/me/ricardo/TesteOperacoesArrays.kt
+++ src/main/kotlin/me/ricardo/TesteOperacoesArrays.kt
@@ -8,6 +8,9 @@ fun main() {
     println("operação com array, Media dos valores: ${salarios.average()}")
     println("operação com array, maximo dos valores: ${salarios.maxOrNull()}")
     println("operação com array, Menor dos valores: ${salarios.minOrNull()}")
+    println("operação com array, valores dentro de um range: ${salarios.count { it in 100.0..5000.0 }}")
+    println("operação com array, encontra um valor igual: ${salarios.find { it == 960.40}}")
+    println("operação com array, encontra um valor igual e retorna boolean: ${salarios.any { it == 1440.30}}")
 
     println("Filter na operação")
     val salarioMaiorQue1000 = salarios.filter { it > 1000.0 }
