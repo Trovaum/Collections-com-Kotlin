06a0c0da95ddc9e210fbbbd23a2a64a52b7ee0e0
Trovaum <ricardopouza@gmail.com>
2022-01-04 09:03:30 -0300

    exercicio DoubleArray/doubleArrayOf

--- src/main/kotlin/me/ricardo/TesteDoubleArray.kt
+++ src/main/kotlin/me/ricardo/TesteDoubleArray.kt
@@ -9,4 +9,15 @@ fun main() {
     for (salario in salarios){
         println(salario)
     }
+
+    println("Usando for each indexado para manipular cada item do array")
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("Usando doubleArrayOf já criando os valores do array")
+    val salario2 = doubleArrayOf(123.10, 440.20, 90.40, 600.80)
+    salario2.sort()
+    salario2.forEach { println(it) }
 }
