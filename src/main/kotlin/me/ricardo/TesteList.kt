a79776c78b248219a811b9bc0e16c513a3a3ece4
Trovaum <ricardopouza@gmail.com>
2022-01-04 18:42:27 -0300

    List groupBy com encadeamento

--- src/main/kotlin/me/ricardo/TesteList.kt
+++ src/main/kotlin/me/ricardo/TesteList.kt
@@ -1,9 +1,9 @@
 package me.ricardo
 
 fun main() {
-    val joao = Funcionario("João", 5000.0)
-    val ricardo = Funcionario("Ricardo", 2000.0)
-    val enzo = Funcionario("Enzo", 3000.0)
+    val joao = Funcionario("João", 5000.0, "CLT")
+    val ricardo = Funcionario("Ricardo", 2000.0, "PJ")
+    val enzo = Funcionario("Enzo", 3000.0, "CLT")
 
     val funcionarios = listOf(joao, ricardo, enzo)
 
@@ -11,12 +11,14 @@ fun main() {
     println("Busca por nome usando metodo find: " + funcionarios.find { it.nome == "Enzo" })
 
     funcionarios.sortedBy { it.salario }.forEach { println("Usando ordenação por faixa salarial " + it) }
+    funcionarios.groupBy { it.tipo }.forEach { println("Usando groupBy por tipo de contratação " + it) }
 
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: String
 ) {
     override fun toString(): String {
         return "Funcionario(nome='$nome', salario=$salario)"
